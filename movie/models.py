16fe22dc2efb4b8afc571636e6c03a2deed6567b
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-05-16 20:28:40 -0400

    Feat: Creado el modelo Movie.
    Feat: Añadida la aplicación movie a las aplicaciones instaladas del proyecto.
    
    Feat: Creada la primera migración del modelo de Movie.
    
    Feat: Registrado el modelo de Movie en el sitio administrativo.

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+
+    name = models.CharField(max_length=150)
+    year = models.PositiveSmallIntegerField()
+    genre = models.CharField(max_length=50, choices=(
+        ('Acc', 'Acción'),
+        ('CicFic', 'Ciencia ficción'),
+        ('Com', 'Comedia'),
+        ('Dram', 'Drama'),
+        ('Fan', 'Fantasía'),
+        ('MelDram', 'Melodrama'),
+        ('Mus', 'Musical'),
+        ('Rom', 'Romance'),
+        ('Susp', 'Suspenso'),
+        ('Terr', 'Terror'),
+        ('Doc', 'Documental'),
+    ))
+    director = models.CharField(max_length=150)
+    staff = models.JSONField()
+    country = models.CharField(max_length=100)
