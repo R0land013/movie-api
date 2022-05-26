1ad48074f3dbe3e8409e39ff8d86d37e5abfa67a
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-05-26 10:24:51 -0400

    Feat: Creado el extra action para obtener las películas filtradas por un país.

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 from movie.models import Movie
 from movie.serializers import MovieSerializer
+from rest_framework.decorators import action
 
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -9,3 +12,12 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [AllowAny]
+
+    @action(detail=False, methods=['GET'], url_path='by_country')
+    def get_movies_by_country(self, request):
+        country = request.data['country']
+
+        movies_by_country = self.queryset.filter(country=country)
+        serializer = MovieSerializer(data=movies_by_country, many=True)
+        serializer.is_valid()
+        return Response(data=serializer.data)
