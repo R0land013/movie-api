b212e70acbc637e47eb9b298083b3752ae970f55
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-05-30 11:20:19 -0400

    Refactor: Cambios para facilitar el entendimiento de los tests. Creada función para realizar peticiones GET usando datos json.

--- tests/movie/test_get_movies_by_country.py
+++ tests/movie/test_get_movies_by_country.py
@@ -3,7 +3,7 @@ from django.test import Client
 from rest_framework.test import APITestCase
 from movie.serializers import MovieSerializer
 from tests.util.generator import generate_movies
-from tests.util.helpers import delete_all_movies_from_database, save_movies_into_database
+from tests.util.helpers import delete_all_movies_from_database, save_movies_into_database, get_request_using_json
 import io
 from rest_framework.parsers import JSONParser
 import json
@@ -22,11 +22,8 @@ class TestGetMoviesByCountry(APITestCase):
         m3.country = 'Cuba'
         save_movies_into_database(movies)
 
-        request_data = json.dumps({'country': 'Cuba'})
-        response = Client().generic('GET', '/movie/by_country/', request_data, 'application/json')
+        response_data = get_request_using_json('/movie/by_country/', {'country': 'Cuba'})
 
-        stream = io.BytesIO(response.content)
-        retrieved_data = JSONParser().parse(stream)
         movie_serializer = MovieSerializer(data=[m1, m3], many=True)
         movie_serializer.is_valid()
-        self.assertEqual(retrieved_data, movie_serializer.data)
+        self.assertEqual(response_data, movie_serializer.data)
--- tests/util/helpers.py
+++ tests/util/helpers.py
@@ -1,4 +1,8 @@
+from rest_framework.parsers import JSONParser
 from movie.models import Movie
+import json
+from django.test import Client
+import io
 
 
 def save_movies_into_database(movies: list):
@@ -10,3 +14,11 @@ def delete_all_movies_from_database():
     movies = Movie.objects.all()
     for a_movie in movies:
         a_movie.delete()
+
+
+def get_request_using_json(url: str, data: dict):
+    request_data = json.dumps(data)
+    response = Client().generic('GET', url, request_data, 'application/json')
+    stream = io.BytesIO(response.content)
+    retrieved_data = JSONParser().parse(stream)
+    return retrieved_data
