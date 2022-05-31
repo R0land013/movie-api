1c20607482f79985e089f26d96a449efa68cb5ef
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-05-31 09:48:51 -0400

    Test: Creada prueba para verificar que se devuelve BAD_REQUEST_400 cuando se realiza una petición errónea a get_movies_by_country.

--- tests/movie/test_get_movies_by_country.py
+++ tests/movie/test_get_movies_by_country.py
@@ -1,12 +1,9 @@
-from django.test import TestCase
-from django.test import Client
 from rest_framework.test import APITestCase
 from movie.serializers import MovieSerializer
 from tests.util.generator import generate_movies
-from tests.util.helpers import delete_all_movies_from_database, save_movies_into_database, get_data_from_GET_request
-import io
-from rest_framework.parsers import JSONParser
-import json
+from tests.util.helpers import delete_all_movies_from_database, save_movies_into_database, get_data_from_GET_request, \
+    get_status_code_from_GET_request
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 
 class TestGetMoviesByCountry(APITestCase):
@@ -27,3 +24,16 @@ class TestGetMoviesByCountry(APITestCase):
         movie_serializer = MovieSerializer(data=[m1, m3], many=True)
         movie_serializer.is_valid()
         self.assertEqual(response_data, movie_serializer.data)
+
+    def test_it_returns_status_BAD_REQUEST_400(self):
+        movies = generate_movies(3)
+        m1, m2, m3 = movies
+        m1.country = 'Cuba'
+        m2.country = 'France'
+        m3.country = 'Cuba'
+        save_movies_into_database(movies)
+
+        response_status = get_status_code_from_GET_request('/movie/by_country/',
+                                                           {'countries': 'Cuba'})
+
+        self.assertEqual(response_status, HTTP_400_BAD_REQUEST)
